8100deaee19ac1e7b69b68055ae2ed09dde79a06
RyoKitatani <ryo0929m@gmail.com>
2021-06-06 08:45:24 +0000

    add controllers

---
+++ app/controllers/rentals_controller.rb
@@ -0,0 +1,7 @@
+class RentalsController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'categories/new'
+  get 'categories/edit'
+  get 'rentals/create'
+  get 'rentals/destroy'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get 'books/new'
   devise_for :users
   root to: 'homes#top'
 end
