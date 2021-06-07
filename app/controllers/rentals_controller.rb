d175059730b452272abee3076e996eb446813f0e
RyoKitatani <ryo0929m@gmail.com>
2021-06-07 22:47:17 +0900

    add incrementメソッド

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,7 +1,13 @@
 class RentalsController < ApplicationController
+
+  def index
+    @rentals = Rental.all
+  end
+
   def create
     @book = Book.find(params[:book_id])
     @rental = current_user.rentals.new(book_id: @book.id, user_id: current_user.id, rental_date: DateTime.now, due_date: DateTime.now + 14 )
+    @book.increment!(:stock_num, -1)
     @rental.save
     redirect_to book_path(@book)
   end
@@ -9,11 +15,9 @@ class RentalsController < ApplicationController
   def destroy
     @book = Book.find(params[:book_id])
     @rental = current_user.rentals.find_by(book_id: @book.id)
+    @book.increment!(:stock_num, 1)
     @rental.destroy
     redirect_to request.referer
   end
 
-  def index
-    @rentals = Rental.all
-  end
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,4 +2,5 @@ class Rental < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
+
 end
